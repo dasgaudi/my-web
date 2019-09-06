79c3fc9904cb94976eaab61adf0cd4c735997bf7
joonhyeokahn <joon.csguy@gmail.com>
2019-09-06 14:51:05 -0500

    tried to center animations

--- src/components/splash.js
+++ src/components/splash.js
@@ -14,7 +14,6 @@ const styles = {
 
 class Splash extends React.Component {
   render() {
-
       return (
           <Animated className="boxContainer" animationIn="rotateIn" animationOut="rotateOut" animationInDuration={1500} animationOutDuration={2500} isVisible={false}>
               <img className="box myimage" src={require("../myarc.svg")} alt="logo"/>
