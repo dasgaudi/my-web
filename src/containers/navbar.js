a49bd3d5135ebb164534f62f313cfa3700aa21e7
joonhyeokahn <joon.csguy@gmail.com>
2019-09-06 15:30:29 -0500

    tried to center animations

--- src/containers/navbar.js
+++ src/containers/navbar.js
@@ -12,6 +12,22 @@ import '../navbar.css';
 
 class Navbar extends React.Component {
 
+  // constructor(props) {
+  //   super(props);
+  //   this.handleToggle = this.handleToggle.bind(this);
+  //   this.state = {
+  //     isOpen: false,
+  //   };
+  // }
+  //
+  // handleToggle() {
+  //   this.setState(prev => {}
+  //     return {
+  //       isOpen: !prev.state.isOpen
+  //     }
+  //   )
+  // }
+
   render() {
     return (
       <Animated animationIn="fadeIn" animationInDuration={3000} isVisible={true}>
@@ -33,7 +49,7 @@ class Navbar extends React.Component {
           <hr/>
 
           <Switch>
-            <Route exact path = '/MyWebsite' component={Home} />
+            <Route exact path = '/' component={Home} />
             <Route path='/experience' component={Experience} />
             <Route path='/project' component={Project} />
             <Route path='/skill' component={Skill} />
