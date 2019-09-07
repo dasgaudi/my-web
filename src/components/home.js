f1661f05978d4c2e7f2cedc885de2901e998c932
joonhyeokahn <joon.csguy@gmail.com>
2019-09-07 13:45:42 -0500

    experimented flex

--- src/components/home.js
+++ src/components/home.js
@@ -5,25 +5,13 @@ import '../home.css';
 class Home extends Component {
   render() {
     return (
-      <div style = {{textAlign: 'center'}}>
-        <div className ="lineHeight boxContainer">
-            <div style = {{ width: '80%'}}className="item">
-              <img src={require(`../myprofile.png`)} alt="my profile" style = {{ width: '90%', borderRadius: '20px'}}/>
-              <br/>
-              <br/>
-              <p style = {{textAlign: 'center', marginLeft: '2%', marginRight: '2%', fontSize: '3vmax', fontWeight: 'bold'}}>Joonhyeok Ahn</p>
-              <p style = {{textAlign: 'center', margin: '0', fontSize: '2.0vmax', fontWeight: 'bold'}}>B.S in Computer Science</p>
-              <br/>
-
-              <div style = {{display: 'flex', justifyContent: 'center'}}>
-                <div className ="nestedItem">
-                  <p className= "missionStatement">_______ Empower people to achieve more by providing software</p>
-                  <br/>
-                  <p className ="description"> There are a lot of problems that can be solved by technologies. My Goal is to create useful software that can help people to solve those problems. To achieve my goal, I've tried to explore and learn a wide range of technologies from web development to mobile devopment, to machine learning recently.</p>
-                </div>
-              </div>
-            </div>
+      <div>
+        <div className= "boxContainer">
+          <img className= "item"src={require(`../myprofile.png`)} alt="my profile" style = {{ width: '40vmin', borderRadius: '20px'}}/>
           <br/>
+          <br/>
+          <p className= "item" style = {{textAlign: 'center', marginLeft: '2%', marginRight: '2%', fontSize: '3vmin', fontWeight: 'bold'}}>Joonhyeok Ahn</p>
+
         </div>
         <Splash/>
       </div>
