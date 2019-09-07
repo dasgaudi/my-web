b7c8cb667dcfc04bd6c1cff241d7869326862a5d
joonhyeokahn <joon.csguy@gmail.com>
2019-09-06 23:52:07 -0500

    added media quries to config proper widths

--- src/components/home.js
+++ src/components/home.js
@@ -8,20 +8,20 @@ class Home extends Component {
       <div style = {{textAlign: 'center'}}>
         <div className ="lineHeight boxContainer">
             <div className="item">
-              <img src={require(`../myprofile.png`)} alt="my profile" style = {{ width: '50vmin', borderRadius: '20px'}}/>
+              <img src={require(`../myprofile.png`)} alt="my profile" style = {{ width: '90%', borderRadius: '20px'}}/>
               <br/>
               <br/>
               <p style = {{textAlign: 'center', marginLeft: '2%', marginRight: '2%', fontSize: '3vmax', fontWeight: 'bold'}}>Joonhyeok Ahn</p>
               <p style = {{textAlign: 'center', margin: '0', fontSize: '2.0vmax', fontWeight: 'bold'}}>B.S in Computer Science</p>
               <br/>
+
               <div style = {{display: 'flex', justifyContent: 'center'}}>
-                <div style = {{width: '50vmin', textAlign: 'left', marginLeft: '2%', marginRight: '2%'}}>
+                <div className ="nestedItem">
                   <p className= "missionStatement">_______ Empower people to achieve more by providing software</p>
                   <br/>
                   <p className ="description"> There are a lot of problems that can be solved by technologies. My Goal is to create useful software that can help people to solve those problems. To achieve my goal, I've tried to explore and learn a wide range of technologies from web development to mobile devopment, to machine learning recently.</p>
                 </div>
               </div>
-
             </div>
           <br/>
         </div>
