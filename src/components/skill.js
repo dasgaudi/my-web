9399fb9531c0a9335c0dd80d4dabbfb4ec147839
joonhyeokahn <joon.csguy@gmail.com>
2019-09-08 09:36:16 -0500

    changed font size of skills page

--- src/components/skill.js
+++ src/components/skill.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 
+import "../css/skills.css";
 class Skill extends Component {
   render() {
     return (
@@ -11,7 +12,7 @@ class Skill extends Component {
 
         <img src={require(`../image/myskills.png`)} alt="my skills" style = {{width: '75%', height: 'auto', display: 'block', margin: 'auto'}}/>
         <br/><br/><br/>
-        <p style = {{textAlign: 'left', fontSize: '1.8vmax', fontWeight: 'bold', marginLeft: '10%', marginRight: '10%'}}>I believe that there are always different skills required to solve various kind of problems. Along the way, curiosity has led me to explore and learn more technologies, in turn, help me to work on various projects to help others solve problems.</p>
+        <p className= "skillsParagraph" style = {{textAlign: 'left', marginLeft: '10%', marginRight: '10%'}}>I believe that there are always different skills required to solve various kind of problems. Along the way, curiosity has led me to explore and learn more technologies, in turn, help me to work on various projects to help others solve problems.</p>
       </div>
     )
   }
