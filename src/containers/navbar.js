211e0cd9bfa41298c3b7cf6dd4f142a44c79bb23
joonhyeokahn <joon.csguy@gmail.com>
2019-10-03 13:26:57 -0500

    try to fix react router?

--- src/containers/navbar.js
+++ src/containers/navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom'
 import {Animated} from "react-animated-css";
 import Home from "../components/home";
